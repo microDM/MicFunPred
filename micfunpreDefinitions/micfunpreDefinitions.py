3be3a39a15bd1aeddf049a5faf8bf648007d4f87
microDM <datta@microdm.net.in>
2022-01-09 12:09:57 +0530

    added support for macOS

--- micfunpreDefinitions/micfunpreDefinitions.py
+++ micfunpreDefinitions/micfunpreDefinitions.py
@@ -34,7 +34,7 @@ def blast(query,db,in_numCores,cwd):
     Returns:
         None
     """
-    if(sys.platform == 'linux'):
+    if(sys.platform in ['darwin','linux','cygwin']):
         cmd = "blastn -out " + os.path.join(cwd,"out.blast") + " -outfmt 6 -query " + query + " -db " + db + " -num_threads " + in_numCores + " -max_target_seqs 1"
     elif(sys.platform == 'win32'):
         cmd = "blastn.exe -out " + os.path.join(cwd,"out.blast") + " -outfmt 6 -query " + query + " -db " + db + " -num_threads " + in_numCores + " -max_target_seqs 1"
